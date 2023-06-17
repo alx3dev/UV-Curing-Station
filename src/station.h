5c8effb3d1dee395c9e842b53ead68bd18e79552
alx3dev <alx3dev@gmail.com>
2023-06-17 20:02:30 +0200

    move buttons_init() to buttons.h

--- src/station.h
+++ src/station.h
@@ -1,22 +1,10 @@
-#include <DigitalSwitch.h>
+#include <OutputSwitch.h>
 
-DigitalSwitch Led( LedPin, ACTIVE_HIGH, AUTO_OFF, 600 );
+OutputSwitch Led( LedPin, ACTIVE_HIGH, AUTO_OFF, 600 );
 
 #ifdef MotorPin
-    DigitalSwitch Motor( MotorPin );
+    OutputSwitch Motor( MotorPin );
 #endif
-
-// Depending on your board and wiring,
-// you may want to initialize buttons with INPUT_PULLUP.
-void buttons_init(byte mode = INPUT) {
-    #ifdef ButtonPin_0
-        pinMode(ButtonPin_0, mode);
-    #endif
-
-    #ifdef ButtonPin_1
-        pinMode(ButtonPin_1, mode);
-    #endif
-}
 
 void uvsON() {
     Led.turnON();
