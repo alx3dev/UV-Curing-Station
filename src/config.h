1a523efda53e1725f230d2bd7463fe2951124486
alx3dev <alx3dev@gmail.com>
2023-06-17 20:04:31 +0200

    update config

--- src/config.h
+++ src/config.h
@@ -1,33 +1,26 @@
 // ** Configuration for UV Station ** //
 
-// Define board chip
-#define ESP_8266    
+#define ESP_8266 // Define board chip
 //#define ESP_32
 
-// Network (or AP) credentials
+// Network credentials
 const char* SSID = "NETWORK-NAME";
 const char* PASS = "NETWORK-PASSWORD";
 
-// UV Led pin
 #define LedPin 3
+#define LedPWM 204          // Default power, comment line to disable led PWM
 
-// Default power, uncomment to disable led PWM
-#define LedPWM 204
+//#define MotorPin 1        // Uncomment to use motor for part rotation
+#define MotorPWM 25.5       // Default speed, comment line to disable motor PWM
 
-// Uncomment if motor is not connected
-#define MotorPin 1
-
-// Default speed, uncomment to disable motor PWM
-#define MotorPWM 25.5
-
-// TO-DO: enable buttons
-//
-//#define ButtonPin_0 2
-//#define ButtonPin_1 4
+//#define ButtonPin 2       // Set button pin number and type
+#define ButtonMode INPUT
+//#define Button1Pin 4
+#define Button1Mode INPUT
 
 #include <station.h>
 
-#if defined(ButtonPin_0) || defined(ButtonPin_1)
+#if defined(ButtonPin) || defined(Button1Pin)
     #define BUTTONS
     #include <buttons.h>
 #endif
