3a62328e5eeafd20a4e8ff5fe569ff83f81c4118
alx3dev <alx3dev@gmail.com>
2023-06-19 22:16:06 +0200

    to-do

--- src/headers/buttons.h
+++ src/headers/buttons.h
@@ -1,11 +1,11 @@
 #ifndef Arduino_h
-    #include <Arduino.h>
+    #include <Arduino.h>    // make VScode happy
 #endif
 
 #ifdef BUTTONS
     // Depending on your board and wiring, you may want to
     // initialize buttons with INPUT_PULLUP or INPUT_PULLDOWN
-    void buttons_init(byte mode = INPUT) {
+    void buttons_init() {
         #ifdef ButtonPin
             pinMode(ButtonPin, ButtonMode);
         #endif
@@ -13,21 +13,24 @@
         #ifdef Button1Pin
             pinMode(Button1Pin, Button1Mode);
         #endif
+
+        #ifdef Button2Pin
+            pinMode(Button2Pin, Button2Mode);
+        #endif
     }
 
-    void buttonPressed() {
+    #ifdef ButtonPin
+        void buttonPressed() {}
+        void buttonReleased() {}
+    #endif
 
-    }
+    #ifdef Button1Pin
+        void button1Pressed() {}
+        void button1Released() {}
+    #endif
 
-    void buttonReleased() {
-
-    }
-
-    void button1Pressed() {
-
-    }
-
-    void button1Released() {
-
-    }
+    #ifdef Button2Pin
+        void button2Pressed() {}
+        void button2Released() {}
+    #endif
 #endif
